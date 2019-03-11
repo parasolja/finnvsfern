e1dd43cb15b2714d339326758d3fc75e1fc5a440
parasolja <parasolja@gmail.com>
2019-03-11 18:33:35 +0100

    Created a basic random board with squares in it (using HTML Canvas. Added progress bars and made some minor visual changes

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function musicOff() {
 
 //Create a board
 
+//First draw a big rectangle using canvas
+
 var board = document.getElementById("board");
 var context = board.getContext("2d");
 
@@ -21,30 +23,120 @@ context.rect(0, 0, 600, 600);
 context.fillStyle = '#e6f4f1';
 context.fill();
 
-class Matrix {
-    constructor(rows, cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.value = [];
+//Add some border
+context.lineWidth = 2;
+context.strokeStyle = '#50a627';
+context.stroke();
 
-        for (var i = 0; i < this.rows; i++) {
-            this.value[i] = []; // fill each row with empty array
-            for (var j = 0; j < this.cols; j++) {   
-                this.value[i][j] = 0; // fill each column with intial value of zero
-            }
-        } 
-    }
-    // function for filling each box with random number
-    randomize() {
-        for (var i = 0; i < this.rows; i++) {
+//Create columns and rows inside of a big rectangle
+ 
+class Layout {
+	constructor(columns,rows) {
+		this.columns = columns;
+		this.rows = rows;
+		this.value = [];
+	
+		for (var i = 0; i < this.rows; i++) {
+	        this.value[i] = []; 
+	        for (var j = 0; j < this.columns; j++) {   
+	            this.value[i][j] = 0; 
+	        }
+		}
+	}
 
-            for (var j = 0; j < this.cols; j++) {
-                this.value[i][j] = Math.floor(Math.random() * Math.floor(5)); 
+	//add some random numbers to boxes
+	getRandom () {
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.columns; j++) {
+                this.value[i][j] = Math.floor(Math.random() * Math.floor(10)); 
             }
         }
     }  
 }
 
-var map = new Matrix(10,10); // create map
-map.randomize(); // fill map with random values
+//Finally create a board and add random values
+
+var board = new Layout (10,10);
+board.getRandom();
+
+//Declare variables for a board (only for squares)
+
+var whiteSquare;
+var whiteSquares = []; //store them all together
+var obsSquare;
+var obsSquares = [];
+var squareSize = 60;
+
+//Create class for squares
+class Square {
+	constructor(row,col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    squareCreate() {
+        context.beginPath();
+        context.rect( this.col * 60, this.row * 60, squareSize, squareSize);  
+    }
+}
+
+//create grid
+
+for (var row = 0; row < board.rows; row++) {
+    
+    for (var col = 0; col < board.columns; col++) {
+    
+        // if box status equals 2, place black box, set box value to 1 (unavailable)
+        if (board.value[row][col] === 2 || board.value[row][col] === 3) {
+
+        	obsSquare = new Square(row, col);
+        	obsSquare.squareCreate();
+            context.fillStyle = '#0099cb';
+            context.fill();
+            board.value[row][col] = 1;
+
+            obsSquares.push(obsSquare); // push all black/unavaiable boxes onto blackBoxes array
+        } else if (board.value[row][col] === 4) { // place something else
+        
+	      	obsSquare = new Square(row, col);
+	    	obsSquare.squareCreate();
+	        context.fillStyle = 'red';
+	        context.fill();
+	        board.value[row][col] = 2;
+        
+    	} else if (board.value[row][col] === 5) { // place something else
+        
+	      	obsSquare = new Square(row, col);
+	    	obsSquare.squareCreate();
+	        context.fillStyle = 'yellow';
+	        context.fill();
+	        board.value[row][col] = 3;
+        
+    	} else { //else place grey box, set box value to 0 (available)
+            
+            whiteSquare = new Square(row, col);
+            whiteSquare.squareCreate();
+            context.fillStyle = 'white';
+            context.fill();
+            context.stroke();
+            board.value[row][col] = 0;
+
+            whiteSquares.push(whiteSquare); // push all grey/available boxes onto greyBoxes array
+        }  
+    }    
+}
+console.table(board.value);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
