f24e15f3d6951d8419740d1c0e165aa98aae16e2
parasolja <parasolja@gmail.com>
2019-03-22 12:24:39 +0100

    Disabled arrow keys scrolling in a browser

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ function musicOff() {
 } 
 
 /**
+ * Disable arrow keys scrolling in a browser
+ */
+var arrow_keys_handler = function(e) {
+    switch(e.keyCode){
+        case 37: case 39: case 38:  case 40: // Arrow keys
+        case 32: e.preventDefault(); break; // Space
+        default: break; // do not block other keys
+    }
+};
+window.addEventListener("keydown", arrow_keys_handler, false);
+
+
+/**
  * World/Playingfield construction
  */
 
